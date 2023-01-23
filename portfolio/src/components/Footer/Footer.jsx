9d0df790ccfaa587f2290a797c3d6aac2cc5b617
wongiro <sonrobin2018@gmail.com>
2023-01-23 12:05:16 +0300

    added the footer components

--- portfolio/src/components/Footer/Footer.jsx
+++ portfolio/src/components/Footer/Footer.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
+import HeaderSocials from '../Header/HeaderSocials'
 import './Footer.css'
+import {SiHashnode} from 'react-icons/si'
+import {BsTwitter} from 'react-icons/bs'
+import {FiInstagram} from 'react-icons/fi' 
 
 const Footer = () => {
   return (
-    <section id='footer'>
-      Footer
-    </section>
+    <footer>
+      <a href='#' className='footer__logo'>ROBINSON</a>
+
+      <ul className='permalinks'>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#about'>About Me</a></li>
+        <li><a href='#experience'>Experience</a></li>
+        <li><a href='#portfolio'>My Projects</a></li>
+        <li><a href='#contact'>Contact Me</a></li>
+      </ul>
+
+      <div className='footer__socials'>
+      <a href='https://instagram.com' target="_blank"><FiInstagram/></a>
+        <a href='https://hashnode.com' target="_blank"><SiHashnode/></a>
+        <a href='https://twitter.com' target="_blank"><BsTwitter/></a>
+      </div>
+
+      <div className='footer__copyright'>
+        <small>&copy; Robinson 'Wonjiro' Rubangakene. All rights reserved</small> 
+      </div>
+    </footer>
   )
 }
 
